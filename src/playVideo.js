26488bd4cf8064c9d11a5a90cf25b9fbaa5ccbf9
Reese Bingham <reese.bingham@aggiemail.usu.edu>
2015-07-28 13:47:24 -0600

    Fixed issue with module name

--- src/playVideo.js
+++ src/playVideo.js
@@ -1,4 +1,4 @@
-angular.module('nanoKeys-web')
+angular.module('videoScroll')
 .directive('playVideo', function ($window){
         // left: 37, up: 38, right: 39, down: 40,
         // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
